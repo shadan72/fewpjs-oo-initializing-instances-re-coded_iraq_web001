66b36ab4949f0393213cd7b851af0f2f08ca8d56
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-12 19:54:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ class Lunch{
   }
 }
 
-class
+class Dinner{
+  constructor(salad,soup,entree,dessert){
+    this.salad=salad;
+    this.soup=soup;
+    this.entree=entree;
+    this._dessert=dessert
+  }
+}
